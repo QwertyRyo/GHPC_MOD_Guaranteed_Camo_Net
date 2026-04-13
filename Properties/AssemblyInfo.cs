0e2461c9dbe19e0b00cb47a765faf2561ebf9e63
QwertyRyo <61341858+QwertyRyo@users.noreply.github.com>
2026-04-13 13:19:15 -0500

    Create AssemblyInfo.cs for GuaranteedCamoNets
    Added assembly information for the GuaranteedCamoNets mod.

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,40 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+using MelonLoader;
+using GuaranteedCamoNets;
+
+[assembly: MelonInfo(typeof(GuaranteedCamoNetsClass), "GuaranteedCamoNets", "1.0.0", "Qwertyryo")]
+[assembly: MelonGame("Radian Simulations LLC", "GHPC")]
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("GuaranteedCamoNets")]
+[assembly: AssemblyDescription("A mod for GHPC that provides a 100% chance of all camo nets spawning on eligible vehicles")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Qwertyryo")]
+[assembly: AssemblyProduct("GuaranteedCamoNets")]
+[assembly: AssemblyCopyright("Copyright ©  2026")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("5d3c618e-9e96-40be-b086-63906f6958a1")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
